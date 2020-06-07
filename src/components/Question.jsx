10dfe1d20895ab8c786fb9cf4178e3f40928530e
Israel Deorce <israel.idvj@gmail.com>
2020-06-07 20:28:48 -0300

    balloon image added on top of options

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import balloon from '../images/balloon.png';
+import './Question.css';
 
 const Question = ({question}) => {
   const errorMessage = 'ERROR: Question not Found! Please, reload the page!';
   const questionText = question || errorMessage;
 
   return (
-    <div id='question-div'>
-      <img id='balloon-img' src={balloon}/>
-      <div id='text-question-div'>{questionText}</div>
+    <div id='question-div' className='question-div'>
+      <img id='balloon-img' className='balloon' src={balloon}/>
+      <div id='text-question-div' className='text-question'>{questionText}</div>
     </div>
   );
 };
