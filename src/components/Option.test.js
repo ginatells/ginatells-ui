146634306fd120fe6bf3b643fef20274ad491026
Israel Deorce <israel.idvj@gmail.com>
2020-06-07 18:55:00 -0300

    Question added and formating issues fixed

--- src/components/Option.test.js
+++ src/components/Option.test.js
@@ -4,43 +4,43 @@ import Option from './Option'
 import { shallow } from 'enzyme';
 
 describe('Option Component', () => {
-    let props;
-    let wrapper;
+  let props;
+  let wrapper;
 
-    const shallowOption = () => {
-        if (!wrapper)
-            wrapper = shallow(<Option {...props} />);
-        return wrapper;
+  const shallowOption = () => {
+    if (!wrapper)
+      wrapper = shallow(<Option {...props} />);
+    return wrapper;
+  };
+
+  beforeEach(() => {
+    props = {
+      text: '',
+      color: '',
+      id: 0,
+      value: 0
     };
+    wrapper = null;
+  });
 
-    beforeEach(() => {
-        props = {
-            text: '',
-            color: '',
-            id: 0,
-            value: 0
-        };
-        wrapper = null;
-    });
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Option {...props} />, div);
+  });
 
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<Option {...props} />, div);
-    });
+  it('renders a given text', () => {
+    props = {
+      ...props,
+      text: 'Very Interested'
+    };
+    expect(shallowOption().find('li').text()).toBe('Very Interested');
+  });
 
-    it('renders a given text', () => {
-        props = {
-            ...props,
-            text: 'Very Interested'
-        };
-        expect(shallowOption().find('li').text()).toBe('Very Interested');
-    });
-
-    it('renders a given color', () => {
-        props = {
-            ...props,
-            color: 'green'
-        };
-        expect(shallowOption().find('li').hasClass('background-green')).toBe(true);
-    });
+  it('renders a given color', () => {
+    props = {
+      ...props,
+      color: 'green'
+    };
+    expect(shallowOption().find('li').hasClass('background-green')).toBe(true);
+  });
 });
