4c28dc0c543c26a12cfc07d4e38bca2da8c7c491
JoaoMoraesJr <joaojr1997@gmail.com>
2021-06-10 21:27:23 -0300

    :package: Updating axios

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,19 +1,27 @@
 import Question from '../components/Question'
 import Gina from '../images/gina.png'
+import questionsService from '../services/questionsService'
 import { ALTERNATIVES } from '../utils/constants'
+import { QUESTIONS } from '../utils/questions'
 
 
 import './Game.scss'
 
 function Game() {
+
+  async function selectOption(text) {
+    console.log(text);
+    await questionsService.postAnswer();
+  }
+
   return (
     <div className='game-container'>
       <img src={Gina} alt='Gina' />
       <div>
         <Question />
         <ul>
-          <li className={'background-green'}>{ALTERNATIVES.HIGHEST}</li>
-          <li className={'background-lightgreen'}>{ALTERNATIVES.HIGH}</li>
+          <li className={'background-green'} onClick={() => selectOption("hello")}>{ALTERNATIVES.HIGHEST}</li>
+          <li className={'background-lightgreen'} onClick={console.log("hi")}>{ALTERNATIVES.HIGH}</li>
           <li className={'background-blue'}>{ALTERNATIVES.MEDIUM}</li>
           <li className={'background-orange'}>{ALTERNATIVES.LOW}</li>
           <li className={'background-red'}>{ALTERNATIVES.LOWEST}</li>
--- src/services/questionsService.js
+++ src/services/questionsService.js
@@ -1,6 +1,28 @@
-function  questionsService () {
-  // TODO: Axios calls
-  console.log('to be defined')
+import axiosConfig from "../axiosConfig"
+
+async function postAnswer() {
+  const bodyMock = {
+    "keywords": [
+      {
+        "keyword": "universe",
+        "weight": 2
+      },
+      {
+        "keyword": "dog",
+        "weight": 2
+      }
+    ]
+  }
+  try {
+    const response = await axiosConfig.post('/Answer', bodyMock);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const questionsService = {
+  postAnswer: (p) => postAnswer(p)
 }
 
 export default questionsService
