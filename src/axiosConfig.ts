3596113025616997ba934978bc8a5dd09f902bf9
Israel Deorce <israel.idvj@gmail.com>
2022-03-26 14:53:22 -0300

    update axios host

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = "http://localhost:5000";
-
+const baseURL = `http://${window.location.hostname}:5000`;
+console.log(window.location.hostname)
 const axiosConfig = axios.create({
   baseURL: baseURL
 });
